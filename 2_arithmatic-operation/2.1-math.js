24455d9b3e6735266cf2447d4cc431169df7b5ea
Pobitro Raj <pobitroraj.07@gmail.com>
2020-09-26 00:12:55 +0600

    JS arithmatic operation

---
+++ 2_arithmatic-operation/2.1-math.js
@@ -0,0 +1,20 @@
+var eggPrice = 6;
+var numberOfEggs = 15;
+var totalCostOfEggs = eggPrice * numberOfEggs;
+
+console.log("Total Cost of Eggs", totalCostOfEggs ,"Taka");
+
+
+//area of circle calculator = pi * r squared
+
+var radius = 10;
+const pi = 3.14159;
+var areaOfCircle = pi * ( radius * radius );
+console.log("Area of Circle is", areaOfCircle, "sq inch");
+
+
+// usd to bdt calculator
+var usdToBdt = 83.5;
+var bdt = 8000;
+var usd = bdt / usdToBdt;
+console.log("$",usd);
---
+++ 2_arithmatic-operation/2.2-math-2.js
@@ -0,0 +1,15 @@
+var x;
+var y = 7;
+
+x = y++;
+// x = y = y + 1; or
+// x = y;
+// x = y + 1;
+
+// x = ++y;
+// x = y + 1 = y; or
+// x = y + 1;
+// x = y;
+
+console.log("x =",x,"y =",y);
+
