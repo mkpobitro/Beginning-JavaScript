e5c412e20cea640135522f6b0ccf8ca3b7b53e51
Pobitro Raj <pobitroraj.07@gmail.com>
2020-09-26 00:13:45 +0600

    javaScript Logic

---
+++ 3_logic/3.1-comparison.js
@@ -0,0 +1,40 @@
+var n = 27;
+if(n<10){
+    console.log("n is less than 10");
+}else if(n > 10 && n < 20 ){
+    console.log("n is less than 20");
+}else{
+    console.log("n is greater than 20");
+} 
+
+
+
+var n = 8;
+if( 3 == n ){
+    console.log("n is equal to 8");
+}else{
+    console.log("n is not equal to 8");
+}
+
+
+
+var personOne = "jamal";
+var personTwo = "Raj";
+var areTheyBro = "true";
+
+if("jamal" == personOne){
+    if("Raj" == personTwo){
+        if(areTheyBro){
+            console.log("They are brother");
+        }else{   
+            console.log("They are not brother");
+        }
+    }
+}
+
+
+
+
+
+
+
