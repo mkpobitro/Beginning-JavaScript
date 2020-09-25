780befaeb17b6673065c7808cdba308fc4a83b93
Pobitro Raj <pobitroraj.07@gmail.com>
2020-09-26 00:11:13 +0600

    JavaScript Introduction

---
+++ 1_js-intro/odd-even.js
@@ -0,0 +1,12 @@
+
+var n = 12;
+var result;
+
+var reminder = n % 2;
+
+if( 0 == reminder ){
+    var result = n+" is a Even number";
+}else{
+    var result = n+" is a Odd number";
+}
+console.log(result);  
---
+++ 1_js-intro/variables-01.js
@@ -0,0 +1,2 @@
+var myName = "Pobitro";
+console.log(myName);
